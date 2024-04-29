f22d1e4db605cb5a2afe0a0387f9d9ab0c3adac3
Likano <117450946+Likano@users.noreply.github.com>
2024-04-29 21:58:19 +0200

    Add files via upload
    Trying texture failed

--- Project1/Main.cpp
+++ Project1/Main.cpp
@@ -1,6 +1,11 @@
 #include<iostream>
+using namespace std;
+
 #include<glad/glad.h>
 #include<GLFW/glfw3.h>
+
+// texture
+#include<stb/stb_image.h>
 
 #include"shaderClass.h"
 #include"VAO.h"
@@ -9,24 +14,21 @@
 
 
 
-// Vertices coordinates
+// Vertices coordinates( Square)
 GLfloat vertices[] =
 {
-	//               coordinates                        colors
-	-0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,      0.8f, 0.3f,  0.02f, // Lower left corner
-	0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,       0.8f, 0.3f,  0.02f, // Lower right corner
-	0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f,    1.0f, 0.6f,  0.32f, // Upper corner
-	-0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f,   0.9f, 0.45f, 0.17f, // Inner left
-	0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f,    0.9f, 0.45f, 0.17f, // Inner right
-	0.0f, -0.5f * float(sqrt(3)) / 3, 0.0f,       0.8f, 0.3f,  0.02f // Inner down
+	// coordinates                colors           cordinates of the texture
+	-0.5f, -0.5f, 0.0f,      1.0f, 0.0f,  0.0f,    0.0f, 0.0f,        //    Lower left corner
+	-0.5f,  0.5f, 0.0f,       0.0f, 1.0f,  0.0f,   0.0f, 1.0f,        //	Upper left corner
+	 0.5f,  0.5f, 0.0f,      0.0f, 0.0f,  1.0f,    1.0f, 1.0f,        //	Upper right corner
+	 0.5f, -0.5f, 0.0f,      1.0f, 1.0f,  1.0f,    1.0f, 0.0f,        //	Lower left corner
 };
 
 // Indices for vertices order
 GLuint indices[] =
 {
-	0, 3, 5, // Lower left triangle
-	3, 2, 4, // Lower right triangle
-	5, 4, 1 // Upper triangle
+	0, 2, 1, // Upper triangle
+	0, 3, 2, // Lower triangle
 };
 
 
@@ -44,20 +46,23 @@ int main()
 	// So that means we only have the modern functions
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	// Create a GLFWwindow object of 800 by 800 pixels, naming it "YoutubeOpenGL"
+	// Create a GLFWwindow object of 800 by 800 pixels, naming it 
 	GLFWwindow* window = glfwCreateWindow(800, 800, "My Practice", NULL, NULL);
+	
 	// Error check if the window fails to create
 	if (window == NULL)
 	{
-		std::cout << "Failed to create GLFW window" << std::endl;
+		cout << "Failed to create GLFW window" << endl;
 		glfwTerminate();
 		return -1;
 	}
+
 	// Introduce the window into the current context
 	glfwMakeContextCurrent(window);
 
 	//Load GLAD so it configures OpenGL
 	gladLoadGL();
+
 	// Specify the viewport of OpenGL in the Window
 	// In this case the viewport goes from x = 0, y = 0, to x = 800, y = 800
 	glViewport(0, 0, 800, 800);
@@ -75,13 +80,50 @@ int main()
 	EBO EBO1(indices, sizeof(indices));
 
 	// Links VBO to VAO
-	VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 6 * sizeof(float), (void*)0);
-	VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 8 * sizeof(float), (void*)0);
+	VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+	VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 8 * sizeof(float), (void*)(6 * sizeof(float)));
 
 	// Unbind all to prevent accidentally modifying them
 	VAO1.Unbind();
 	VBO1.Unbind();
 	EBO1.Unbind();
+
+	GLuint uniID = glGetUniformLocation(shaderProgram.ID, "scale");
+
+	// Texture
+
+	int widthImg, heightImg, numColCh;
+	unsigned char* bytes = stbi_load("pop_cat.png", &widthImg, &heightImg, &numColCh, 0);
+
+	// creating a reference variable for texture
+	GLuint texture;
+	glGenTextures(1, &texture);
+
+	// slots of textures that come as a bundle
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, texture);
+
+	// adjusting settings of the texture(gl_nearest= Same as they are, create according to nearby pixels)
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+	// How we want texture tobe repeated
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+	// Generating a texture (RGB = jpg, RGBA = png)
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, widthImg, heightImg, 0, GL_RGBA, GL_UNSIGNED_BYTE, bytes);
+	glGenerateMipmap(GL_TEXTURE_2D);
+
+	// deleting data
+	stbi_image_free(bytes);
+	glBindTexture(GL_TEXTURE_2D, 0);
+
+	// create  the uniform
+	GLuint tex0Uni = glGetUniformLocation(shaderProgram.ID, "tex0");
+	shaderProgram.Activate();
+	glUniform1i(tex0Uni, 0);
 
 
 
@@ -90,14 +132,22 @@ int main()
 	{
 		// Specify the color of the background
 		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+
 		// Clean the back buffer and assign the new color to it
 		glClear(GL_COLOR_BUFFER_BIT);
+
 		// Tell OpenGL which Shader Program we want to use
 		shaderProgram.Activate();
+
+		// only after activation
+		glUniform1i(uniID, 0.5f);
+
+		glBindTexture(GL_TEXTURE_2D, texture);
+
 		// Bind the VAO so OpenGL knows to use it
 		VAO1.Bind();
 		// Draw primitives, number of indices, datatype of indices, index of indices
-		glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 		// Swap the back buffer with the front buffer
 		glfwSwapBuffers(window);
 		// Take care of all GLFW events
@@ -108,6 +158,7 @@ int main()
 	VAO1.Delete();
 	VBO1.Delete();
 	EBO1.Delete();
+	glDeleteTextures(1, &texture);
 	shaderProgram.Delete();
 
 	// Delete window before ending the program
--- Project1/default.vert
+++ Project1/default.vert
@@ -1,13 +1,29 @@
 #version 330 core
+
+// Positions/ cordinates
 layout (location = 0) in vec3 aPos;
+
+// colors
 layout (location = 1) in vec3 aColor; 
 
+layout (location = 2) in vec2 aTex;
+
+// outputs the color for the fragment shader
 out vec3 color;
 
+
+out vec2 textCoord;
+
+// controls the scale
 uniform float scale;
 
-void main()
+void main() 
 {
+	// output the positions of all vertices
 	gl_Position = vec4(aPos.x + aPos.x * scale , aPos.y + aPos.y * scale, aPos.z + aPos.z * scale, 1.0);
+	
+	// control the colors from the vertex data to color
 	color = aColor;
+
+	textCoord = aTex;
 }
